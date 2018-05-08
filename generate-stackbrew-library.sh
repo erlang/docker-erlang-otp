e340ee31e13370a857189da0c14a69f364a9861a
getong <getong@users.noreply.github.com>
2018-05-08 12:44:49 +0800

    change git repo address

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -48,10 +48,10 @@ extractVersion() {
 self="${BASH_SOURCE##*/}"
 
 cat <<-EOH
-# this file is generated via https://github.com/c0b/docker-erlang-otp/blob/$(fileCommit "$self")/$self
+# this file is generated via https://github.com/erlang/docker-erlang-otp/blob/$(fileCommit "$self")/$self
 
 Maintainers: Mr C0B <denc716@gmail.com> (@c0b)
-GitRepo: https://github.com/c0b/docker-erlang-otp.git
+GitRepo: https://github.com/erlang/docker-erlang-otp.git
 EOH
 
 for version in "${versions[@]}"; do
