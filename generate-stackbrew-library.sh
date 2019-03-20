b30a46da1409c058f16d6df02310134c65b5236f
getong <3949379+getong@users.noreply.github.com>
2019-03-20 16:32:08 +0800

    add arm32v7 for erlang/otp 20+

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -84,9 +84,7 @@ for version in "${versions[@]}"; do
 		fi
 
 		variantArches=( amd64 arm32v7 arm64v8 i386 s390x ppc64le )
-		case "$variant" in
-			alpine) variantArches=( ${variantArches[@]/arm32v7} )
-		esac
+
 		case "$version" in
 		        19|18) variantArches=( ${variantArches[@]/ppc64le} )
 		esac
