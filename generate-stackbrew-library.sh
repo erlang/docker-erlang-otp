7ad81df8943ef6a5b15b8e4839ef45474f8760b0
19年梦醒 <3949379+getong@users.noreply.github.com>
2020-07-23 09:51:19 +0800

    Since debian:stretch in now LTS, it no longer supports ppc64le or s390x.

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -86,7 +86,9 @@ for version in "${versions[@]}"; do
 		variantArches=( amd64 arm32v7 arm64v8 i386 s390x ppc64le )
 
 		case "$version" in
-		        19|18) variantArches=( ${variantArches[@]/ppc64le} )
+		    21|20|19|18)
+				variantArches=( ${variantArches[@]/s390x} )
+				variantArches=( ${variantArches[@]/ppc64le} )
 		esac
 
 		echo
